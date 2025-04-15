490208c6ad15bdaf47be5cb21ba697aab3e0bf27
TakeJoshyy <125616429+TakeJoshyy@users.noreply.github.com>
2025-04-15 01:21:43 +0100

    Back To The Top Button

--- docs/index.js
+++ docs/index.js
@@ -65,6 +65,20 @@ function $(id) { return document.getElementById(id) }
     window.onhashchange();
 })();
 
+const backToTopBtn = document.getElementById('backToTopBtn');
+
+window.onscroll = function() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+};
+
+backToTopBtn.onclick = function() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+};
+
 // Currently the search function is broken \_0_/
 
 /*
